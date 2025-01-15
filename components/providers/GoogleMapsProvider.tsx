287cf315cbbc88ad33376d52bb64746b32bf99f0
Hung Tran (Harvin) <hung.tran@workforceoptimizer.com>
2025-01-15 13:04:03 +0700

    Change env key

--- components/providers/GoogleMapsProvider.tsx
+++ components/providers/GoogleMapsProvider.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { config } from '@/lib/config';
+// import { config } from '@/lib/config';
 import { APIProvider } from '@vis.gl/react-google-maps';
 
+const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 export function GoogleMapsProvider({ children }: { children: React.ReactNode }) {
   return (
-    <APIProvider apiKey={config.googleMapsApiKey}>
+    <APIProvider apiKey={apiKey!}>
       {children}
     </APIProvider>
   );
